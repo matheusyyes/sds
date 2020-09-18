a755435730e9376f1513b039b8b91ad02f66c5ef
Miranda <matheus.300miranda@live.com>
2020-09-18 18:51:17 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dspesquisa.entities.enums.Platform;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_game")
@@ -27,6 +28,7 @@ public class Game implements Serializable{
 	private long id;
 	private String title;
 	private Platform platform;
+	
 	
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
