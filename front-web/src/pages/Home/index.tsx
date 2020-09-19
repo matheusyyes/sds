574b0b3f8f958c8c18ae1f2e4c3a0c3eac0ba4e1
Miranda <matheus.300miranda@live.com>
2020-09-19 14:37:12 -0300

    Criando navegação com rotas

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 
-import {ReactComponent as Seta} from '../../assets/seta.svg';
-import {ReactComponent as Gamer} from '../../assets/gamer.svg';
+import { ReactComponent as Seta } from '../../assets/seta.svg';
+import { ReactComponent as Gamer } from '../../assets/gamer.svg';
+import { Link } from 'react-router-dom'
 import './styles.css';
 
-const Home = () =>(
+const Home = () => (
     <div className="home-container">
         <div className="home-text">
             <h1 className="home-text-title">
-            Quais jogos a galera gosta mais?
-            </h1>   
+                Quais jogos a galera gosta mais?
+            </h1>
             <h3 className="home-text-subtitle">
-            Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
-            </h3> 
-            <div className="home-actions">
-                <button className="home-btn">
-                    QUERO SABER QUAIS SÃO
+                Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
+            </h3>
+            <Link to="/records">
+                <div className="home-actions">
+                    <button className="home-btn">
+                        QUERO SABER QUAIS SÃO
                 </button>
-             <div className="home-btn-icon">
-                    <Seta></Seta>
-            </div>
-         </div>
-        </div> 
-        <Gamer className="home-image"></Gamer>     
+                    <div className="home-btn-icon">
+                        <Seta></Seta>
+                    </div>
+                </div>
+            </Link>
+        </div>
+        <Gamer className="home-image"></Gamer>
     </div>
 );
 
